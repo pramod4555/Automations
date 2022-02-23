a74847eaffa21bc1d5557c1a8bfa11f046d75df7
pramod4555 <41017583+pramod4555@users.noreply.github.com>
2022-02-22 23:00:54 -0800

    updated regions
    regions details updated

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-04656078adf4aa403"
   instance_type = "t3.micro"
+  region  = "us-east-1"
   vpc_security_group_ids = [aws_security_group.SG1.id]
  tags = {
     Name = "Devops"
